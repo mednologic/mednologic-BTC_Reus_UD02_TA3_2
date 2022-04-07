c9bcb7391365378bf225434c3998b66086ba9c09
mednologic <mednologic@gmail.com>
2022-04-07 12:08:12 +0200

    "Structure and files created and operative"

---
+++ BTC_Reus_UD02_TA3_2/src/Pr02/ComentarioLinea.java
@@ -0,0 +1,10 @@
+package Pr02;
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		//System.out.println("Hola");
+		  System.out.println("Adios");  
+		  System.out.println("FIN DE PROGRAMA");
+	}
+
+}
